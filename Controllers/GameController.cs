e54dbb29664b7fb69a43d6dc626b457f3a590898
Kjell Einar Mikkelsen <kem@statoil.com>
2020-10-29 16:04:45 +0100

    Add GamesController and solution

---
+++ Controllers/GameController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using gathering_gaia.Models;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace gathering_gaia.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GameController : ControllerBase
+    {
+        // GET: api/<GameController>
+        [HttpGet]
+        public IEnumerable<Game> Get()
+        {
+            return new Game[0];
+        }
+
+        // GET api/<GameController>/5
+        [HttpGet("{id}")]
+        public Game Get(int id)
+        {
+            return new Game();
+        }
+
+        // POST api/<GameController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<GameController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<GameController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
