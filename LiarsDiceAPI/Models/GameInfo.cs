b4fb1b4eaaf76225f25c907368da5906b677a98c
Terje <terjebol@gmail.com>
2020-10-30 09:13:15 +0100

    Added GameInfo.cs that is a lightweight object sendt to the client. Also added name to Game.cs

---
+++ LiarsDiceAPI/Models/GameInfo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace LiarsDiceAPI.Models
+{
+    public class GameInfo
+    {
+        public int PlayersJoined { get; set; }
+        public int MaxPlayers { get; set; } = Game.MaxPlayers;
+        public string Name { get; set; }
+        public string CreatedBy { get; set; }
+        public Guid Id { get; set; }
+    }
+}
